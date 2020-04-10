7c109977abbba3219f6d29bef9b1f0f532a90db2
Jagpreet Singh <jagpreet.singh@capco.com>
2020-04-10 15:29:55 -0400

    Added table component

---
+++ src/app/elements/elements.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { TableComponent } from './table/table.component';
+
+@NgModule({
+  declarations: [TableComponent],
+  imports: [CommonModule]
+})
+export class ElementsModule {}
