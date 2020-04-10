c13480c0e13c9ba7bf5b02929a042521d434cd43
Jagpreet Singh <jagpreet.singh@capco.com>
2020-04-10 19:01:15 -0400

    connected paginator

--- src/app/user/components/table/table.component.ts
+++ src/app/user/components/table/table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 
 import { UserHttpService } from '../../services/user.http.service';
 import { UserDataSource } from './table.datasource';
@@ -8,15 +9,26 @@ import { UserDataSource } from './table.datasource';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, AfterViewInit {
   displayedColumns = ['id', 'firstName', 'lastName'];
 
   dataSource: UserDataSource;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private userHttp: UserHttpService) {}
 
   ngOnInit() {
     this.dataSource = new UserDataSource(this.userHttp);
     this.dataSource.loadUsers({ index: 0, size: 3 });
+  }
+
+  ngAfterViewInit() {
+    this.paginator.page.subscribe(() =>
+      this.dataSource.loadUsers({
+        index: this.paginator.pageIndex,
+        size: this.paginator.pageSize
+      })
+    );
   }
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 
 import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [TableComponent],
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule],
   exports: [TableComponent]
 })
 export class UserModule {}
