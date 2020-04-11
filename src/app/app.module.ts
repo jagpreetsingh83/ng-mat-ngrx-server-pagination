c8e892e3ae2d0d29a26970da94b638cbafed58ff
Jagpreet Singh <jagpreet.singh@capco.com>
2020-04-11 11:02:15 -0400

    Added store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 
@@ -12,7 +18,26 @@ import { UserModule } from './user/user.module';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    UserModule
+    UserModule,
+    RouterModule.forRoot([]),
+    StoreModule.forRoot(
+      {
+        router: routerReducer
+      },
+      {
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true
+        }
+      }
+    ),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production // Restrict extension to log-only mode
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   bootstrap: [AppComponent]
 })
