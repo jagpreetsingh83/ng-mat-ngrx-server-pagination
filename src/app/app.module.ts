d907c1fb3be01026540a5918d6fc8d851d757338
Jagpreet Singh <jagpreet.singh@capco.com>
2020-04-11 11:03:36 -0400

    Removed router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
-import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
@@ -19,11 +17,8 @@ import { UserModule } from './user/user.module';
     BrowserAnimationsModule,
     HttpClientModule,
     UserModule,
-    RouterModule.forRoot([]),
     StoreModule.forRoot(
-      {
-        router: routerReducer
-      },
+      {},
       {
         runtimeChecks: {
           strictStateImmutability: true,
@@ -32,7 +27,6 @@ import { UserModule } from './user/user.module';
       }
     ),
     EffectsModule.forRoot([]),
-    StoreRouterConnectingModule.forRoot(),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production // Restrict extension to log-only mode
